e7de4ca49cbcedaeea1b45cb2cc222bb98d70419
rayssakesia <rayssakesialo@gmail.com>
2025-09-21 13:06:26 -0300

    "adicionando configs, controller, requests"

--- src/main/kotlin/voz_do_povo_api/controller/VozDoPovoController.kt
+++ src/main/kotlin/voz_do_povo_api/controller/VozDoPovoController.kt
@@ -1,4 +1,65 @@
 package voz_do_povo_api.controller
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import voz_do_povo_api.controller.requests.PublicationRequest
+import voz_do_povo_api.controller.requests.ReportAddressRequest
+import voz_do_povo_api.controller.requests.ReportRequest
+import voz_do_povo_api.controller.requests.UserRequest
+
+@RestController
+@RequestMapping(value = ["/voz-do-povo"])
 class VozDoPovoController {
+
+    @PostMapping("/publish")
+    @ResponseStatus (HttpStatus.CREATED)
+    fun publish(@RequestBody publicationRequest: PublicationRequest): PublicationRequest {
+        return PublicationRequest(UserRequest(email = "joao@gmail.com"),
+            ReportAddressRequest(
+                number = "123",
+                zipCode = "123",
+                street ="123",
+                complement = "123",
+                city = "123",
+                state = "123",
+                country = "123"
+            ),
+            ReportRequest(report = "falta de iluminação na rua")
+        )
+    }
+
+    @GetMapping("/email") //(/id user)
+    @ResponseStatus (HttpStatus.OK)
+    fun findPublications(): PublicationRequest {
+        return PublicationRequest(UserRequest(email = "joao@gmail.com"),
+            ReportAddressRequest(
+                number = "123",
+                zipCode = "123",
+                street ="123",
+                complement = "123",
+                city = "123",
+                state = "123",
+                country = "123"
+            ),
+            ReportRequest(report = "falta de iluminação na rua")
+        )
+    }
 }
+
+//@Serializable
+//data class Message (val name: String)
+//
+//@RestController
+//class ControllerTeste {
+//    val m: Message = Message(name = "ready")
+//
+//    @GetMapping("/users")
+//    fun getAnswer(): Message {
+//        return m
+//    }
+//}
--- src/main/kotlin/voz_do_povo_api/controller/requests/publicationRequest.kt
+++ src/main/kotlin/voz_do_povo_api/controller/requests/publicationRequest.kt
@@ -1,3 +1,47 @@
 package voz_do_povo_api.controller.requests
 
-data class publicationRequest()
+import kotlinx.serialization.Serializable
+import org.jetbrains.annotations.NotNull
+
+@Serializable
+data class PublicationRequest(
+
+    val userRequest: UserRequest,
+
+    val reportAddressRequest: ReportAddressRequest,
+
+    val report: ReportRequest
+
+)
+
+@Serializable
+data class UserRequest(
+
+    val name: String? = "Usuário Anônimo",
+    @NotNull
+    val email: String
+)
+
+@Serializable
+data class ReportAddressRequest(
+
+    @NotNull
+    val number: String,
+    @NotNull
+    val zipCode: String,
+    @NotNull
+    val street: String,
+    val complement: String?,
+    @NotNull
+    val city: String,
+    @NotNull
+    val state: String,
+    @NotNull
+    val country: String
+)
+
+@Serializable
+data class ReportRequest(
+    @NotNull
+    val report: String,
+)
